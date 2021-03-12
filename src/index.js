977c5c9dc6b0ad1848d31c17938be55ef2a88aff
vlad23349 <78217932+vlad23349@users.noreply.github.com>
2021-03-12 19:35:14 +0300

    feat: upload solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let str = n.toString();
+    let str1 = '';
+    for (let i = 0; i < str.length; i++) {
+        str1 += str[str.length - (i + 1)];
+    }
+    return parseInt(str1);
 }
